573c1997af8df434a5fe91a27647216fc639aac5
Ares-Source <103866581+lomadinlong@users.noreply.github.com>
2026-01-25 10:36:04 -0500

    import files into github

---
+++ include/subsystems.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "EZ-Template/api.hpp"
+#include "api.h"
+
+extern Drive chassis;
+
+// Your motors, sensors, etc. should go here.  Below are examples
+inline pros::MotorGroup intakebottom({-10,9});
+inline pros::Motor intakebottom2(-8);
+inline pros::Motor intakemiddle(7);
+inline pros::Motor intaketop(6);
+inline pros::Motor intaketop2(5);
+// inline pros::Motor intake(1);
+// inline pros::adi::DigitalIn limit_switch('A');
